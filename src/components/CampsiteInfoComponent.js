5329b5f600ec9e5586c6cc00d8635f5d60ff8951
Joseph Rubino <joeyrubi22@gmail.com>
2022-01-29 12:23:16 -0800

    About Component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,7 +43,7 @@ function CampsiteInfo(props) {
         return (
             <div className="row">
                 <RenderCampsite campsite={props.campsite} />
-                <RenderComments comments={props.campsite.comments} />
+                <RenderComments comments={props.comments} />
             </div>
         )
     } else {
